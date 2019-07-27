9ed6a490ff89658265f941238ed592cdfebf614c
Israel Muñoz <isra.munoz@gmail.com>
2019-07-27 10:35:34 -0500

    Feat: Added the Causes section with its scss fonts in main.scss

---
+++ components/index/Causes.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="columns">
+    <section class="column is-12 causes">
+      <div class="box" v-if="this.curEsp">
+        <h2 class="box-title">Causes</h2>
+
+        <div class="cause" v-for="(c, k) in curEsp.causes" :key="k">
+          <p class="cause-name">{{ c.name }}</p>
+          <p class="cause-reason">{{ c.reason }}</p>
+          <br />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+export default {
+  computed: {
+    ...mapState(["curEsp"])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/main.scss";
+
+.causes {
+  width: 94.5% !important;
+
+  .box-title {
+    @include box-title;
+  }
+
+  .cause {
+    &:not(:last-child) {
+      margin-bottom: 20px;
+    }
+
+    .cause-name {
+      @include cause-name;
+
+      padding-left: 5px;
+    }
+
+    .cause-reason {
+      @include cause-reason;
+
+      padding-left: 10px;
+    }
+  }
+}
+</style>
