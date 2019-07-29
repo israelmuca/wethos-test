e54d7a419bcda5620d1f7df5377455b111d1a35e
Israel Muñoz <isra.munoz@gmail.com>
2019-07-28 20:27:25 -0500

    Feat: Added an individual screen for projects, currently, I only have one, so for testing I cloned it several times, it is working as expected. Also the project has very little info, so not much to show. Will add more data

--- components/projects/Projects.vue
+++ components/projects/Projects.vue
@@ -1,9 +1,10 @@
 <template>
   <main class="projects">
-    <h1 class="projects-page-title">Projects</h1>
+    <h1 class="projects-page-title" v-if="!showIndPro">Projects</h1>
 
+    <!-- Show all projects in boxes -->
     <section class="projects columns is-multiline">
-      <section class="column is-6 project" v-for="(p,k) in projects" :key="k">
+      <section class="column is-6 project" v-for="(p,k) in projects" :key="k" v-if="!showIndPro">
         <div class="box">
           <h2 class="project-name">{{ p.name }}</h2>
           <p class="project-organization">{{ p.organization.name }}</p>
@@ -11,13 +12,25 @@
           <p class="project-statement-purpose" v-html="p.statement_of_purpose"></p>
 
           <div class="columns is-mobile options-columns">
-            <div class="column is-4 see-more">See more</div>
+            <div class="column is-4 see-more" @click="showIndProMet(p.id)">See more</div>
             <div
               class="column is-8 has-text-right"
             >{{ p.specialists[0].user.first_name }} {{ p.specialists[0].user.last_name }}</div>
           </div>
         </div>
       </section>
+    </section>
+
+    <!-- Show an individual project in full screen -->
+    <section class="columns">
+      <div class="column is-12 full-screen-project">
+        <div class="box" v-if="showIndPro">
+          <div class="close-icon has-text-right" @click="hideIndProMet()">X</div>
+          <h1 class="project-title">{{ indProj.name }}</h1>
+          <p class="project-organization">{{ indProj.organization.name }}</p>
+          <p class="project-description" v-html="indProj.description"></p>
+        </div>
+      </div>
     </section>
   </main>
 </template>
@@ -26,8 +39,33 @@
 import { mapState } from "vuex"
 
 export default {
+  data() {
+    return {
+      showIndPro: false,
+      indProj: {}
+    }
+  },
+
   computed: {
     ...mapState(["projects"])
+  },
+
+  methods: {
+    showIndProMet(id) {
+      // Find the project that was selected
+      this.indProj = this.projects.find(p => p.id == id)
+
+      // Remove the individual projects and activate the individual one
+      this.showIndPro = true
+    },
+
+    hideIndProMet() {
+      // Erase the individual Project
+      this.indProj = {}
+
+      // Hide the individual project and show all
+      this.showIndPro = false
+    }
   }
 }
 </script>
@@ -68,6 +106,33 @@ export default {
     @include see-more;
 
     cursor: pointer;
+  }
+
+  /* Individual full screen project box */
+
+  .full-screen-project {
+    margin-top: 5vw;
+
+    .close-icon {
+      margin-top: -10px;
+      padding-left: 79vw;
+      width: 5vw;
+      cursor: pointer;
+    }
+
+    .project-title {
+      @include ind-project-name;
+      margin-bottom: 7.5px;
+    }
+
+    .project-organization {
+      @include ind-project-organization;
+      margin-bottom: 20px;
+    }
+
+    .project-description {
+      @include ind-project-description;
+    }
   }
 }
 </style>
