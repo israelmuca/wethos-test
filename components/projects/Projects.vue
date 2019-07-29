d7958a4759b173c82c299c04bb3b23aa7b6d4c84
Israel Muñoz <isra.munoz@gmail.com>
2019-07-28 19:19:10 -0500

    Feat: Styled the project card, missing the 'see-more' option

--- components/projects/Projects.vue
+++ components/projects/Projects.vue
@@ -1,20 +1,19 @@
 <template>
   <main class="projects">
-    <h1 class="projects-title">Projects</h1>
+    <h1 class="projects-page-title">Projects</h1>
 
     <section class="projects columns is-multiline">
       <section class="column is-6 project" v-for="(p,k) in projects" :key="k">
         <div class="box">
           <h2 class="project-name">{{ p.name }}</h2>
           <p class="project-organization">{{ p.organization.name }}</p>
-          <p class="project-description" v-html="p.description">
-            <!-- {{ p.description }} -->
-          </p>
+          <p class="project-description" v-html="p.description"></p>
+          <p class="project-statement-purpose" v-html="p.statement_of_purpose"></p>
 
-          <div class="columns is-mobile">
-            <div class="column is-6">See more</div>
+          <div class="columns is-mobile options-columns">
+            <div class="column is-4 see-more">See more</div>
             <div
-              class="column is-6"
+              class="column is-8 has-text-right"
             >{{ p.specialists[0].user.first_name }} {{ p.specialists[0].user.last_name }}</div>
           </div>
         </div>
@@ -37,45 +36,38 @@ export default {
 @import "~/assets/css/main.scss";
 
 .projects {
-  width: 94.5% !important;
+  .projects-page-title {
+    @include projects-page-title;
 
-  .box-title {
-    @include box-title;
+    margin-top: 5vw;
+    margin-bottom: 2.5vw;
   }
 
-  .card-header {
-    display: flex;
+  .project-name {
+    @include project-name;
+  }
+
+  .project-organization {
+    @include project-organization;
+    margin-bottom: 10px;
+  }
+
+  .project-description {
+    @include project-description;
+  }
+
+  .project-statement-purpose {
+    @include project-description;
+    font-style: italic;
+
+    margin-top: 3px;
+    margin-bottom: 13px;
+  }
+
+  .see-more {
+    @include see-more;
 
     cursor: pointer;
-
-    .card-header-icon {
-      max-height: 18px;
-
-      svg {
-        margin-left: 20px;
-
-        height: 18px;
-        width: 18px;
-      }
-    }
-  }
-  .category {
-    .category-title {
-      @include category-title;
-    }
-
-    .category-item {
-      @include category-item;
-
-      padding-left: 5px;
-      margin-bottom: 2px;
-
-      &::before {
-        display: inline-block;
-        content: "-";
-        padding-right: 3px;
-      }
-    }
   }
 }
 </style>
